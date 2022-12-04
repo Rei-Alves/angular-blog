ebab4ed27045c488261dd8b16a244b87cfc470c4
Rei-2020 <renildo.b.alves@gmail.com>
2022-12-04 17:06:46 -0300

    content page dinamica

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Marvel anuncia um nova serie Avangers, confira",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+
+
+   {
+    "id":"3",
+    "title":"Novo filme do pantera negra lançado em breve",
+    "description": "Marvel anuncia um novo filme do Pantera negra: Wakanda Para Sempre , confira",
+     "photoCover":"https://legadodamarvel.com.br/wp-content/uploads/2022/02/pantera-negra-2-atriz-posta-nova-foto-com-todo-o-elenco-do-filme-legadodamarvel.jpg"
+  },
+
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "Por mais que ainda não haja uma previsão de estreia para o próximo filme dos mutantes, já podemos esperar que diversidade será um assunto importante para a nova versão no MCU",
+     "photoCover":"https://legadodamarvel.com.br/wp-content/uploads/2021/11/Marvel-finalmente-fala-sobre-como-sera-o-proximo-filme-dos-X-Men-legadodamarvel-750x422.jpg.webp"
+  }
+
+
+
+]
